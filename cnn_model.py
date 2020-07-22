8249058ceedfe4b0ba99bf8311555caba4a342f5
dom silva <dmzksh@gmail.com>
2020-07-22 16:33:34 +0400

    remove dropout layer at end of each branch

--- cnn_model.py
+++ cnn_model.py
@@ -22,27 +22,27 @@ def create_model(input_length, num_class, vocab_size, embed_dim,
     batch_0 = BatchNormalization()(activ_0)
     conv_0_1 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embed_dim))(batch_0)
     activ_0_1 = Activation('relu')(conv_0_1)
-    maxp_0 = MaxPool2D(pool_size=(input_length - filter_sizes[0]+1, 1))(activ_0_1)
+    maxp_0 = MaxPool2D(pool_size=(input_length-filter_sizes[0]+1, 1))(activ_0_1)
 
     conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embed_dim), padding='same')(reshape)
     activ_1 = Activation('relu')(conv_1)
     batch_1 = BatchNormalization()(activ_1)
     conv_1_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embed_dim))(batch_1)
     activ_1_1 = Activation('relu')(conv_1_1)
-    maxp_1 = MaxPool2D(pool_size=(input_length - filter_sizes[1]+1, 1))(activ_1_1)
+    maxp_1 = MaxPool2D(pool_size=(input_length-filter_sizes[1]+1, 1))(activ_1_1)
 
     conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embed_dim), padding='same')(reshape)
     activ_2 = Activation('relu')(conv_2)
     batch_2 = BatchNormalization()(activ_2)
     conv_2_1 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embed_dim))(batch_2)
     activ_2_1 = Activation('relu')(conv_2_1)
-    maxp_2 = MaxPool2D(pool_size=(input_length - filter_sizes[2]+1, 1))(activ_2_1)
+    maxp_2 = MaxPool2D(pool_size=(input_length-filter_sizes[2]+1, 1))(activ_2_1)
 
     concat_tensor = Concatenate(axis=1)([maxp_0, maxp_1, maxp_2])
     flatten = Flatten()(concat_tensor)
-    dense = Dense(units=32, activation='relu')(flatten)
-    dropout = Dropout(drop)(dense)
-    output = Dense(units=num_class, activation='softmax')(dropout)
+    #dense = Dense(units=64, activation='relu')(flatten)
+    dropout = Dropout(drop)(flatten)
+    output = Dense(units=num_class, activation='sigmoid')(dropout)
 
     # this creates a model that includes
     model = Model(inputs=inputs, outputs=output)
@@ -51,6 +51,19 @@ def create_model(input_length, num_class, vocab_size, embed_dim,
     opt = SGD(lr=1e-4, decay=1e-8, momentum=0.9, nesterov=True)
 
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+def create_model2(input_length, num_class, vocab_size, embed_dim, drop=0.5):
+
+    model = Sequential()
+    model.add(Embedding(input_dim=vocab_size, output_dim=embed_dim, input_length=input_length))
+    model.add(Dropout(drop))
+    model.add(Conv1D(64, 5, activation='relu'))
+    model.add(MaxPooling1D(pool_size=4))
+    model.add(LSTM(embed_dim, dropout=drop, recurrent_dropout=drop))
+    model.add(Dense(num_class, activation='sigmoid'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam',    metrics=['accuracy'])
 
     return model
 
